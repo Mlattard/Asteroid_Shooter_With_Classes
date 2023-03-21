f84f896e0264c1b0570923ff2179b1f10ac88e76
Mlattard <maxime@lattard.fr>
2023-03-21 19:18:53 -0400

    Sprite
    - How to use sprite

--- asteroid_shooter_classes.py
+++ asteroid_shooter_classes.py
@@ -1,11 +1,34 @@
 import pygame, sys
 
+class Ship(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load('graphics/ship.png').convert_alpha()
+        self.rect = self.image.get_rect(center = (WINDOW_WIDTH /2, WINDOW_HEIGHT /2))
+
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, groups, ship):
+        super().__init__(groups)
+        self.image = pygame.image.load('graphics/laser.png').convert_alpha()
+        self.rect = self.image.get_rect(midbottom = ship.rect.midtop)
+
 # basic setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Poew Poew Poew with CLASS")
 clock = pygame.time.Clock()
+
+# background
+background_surface = pygame.image.load('graphics/background.png').convert()
+
+# sprite groups
+spaceship_group = pygame.sprite.GroupSingle()
+laser_group = pygame.sprite.Group()
+
+# sprite creation
+ship = Ship(spaceship_group)
+laser = Laser(laser_group, ship)
 
 # game loop
 while True:
@@ -18,6 +41,13 @@ while True:
     
     # delta time
     dt = clock.tick() / 1000
+
+    # backgrounds
+    display_surface.blit(background_surface, (0,0))
+
+    # graphics
+    spaceship_group.draw(display_surface)
+    laser_group.draw(display_surface)
 
     # draw the frame
     pygame.display.update()

efeae19aae787220505f171f3c5e1331b8bdf045
Mlattard <maxime@lattard.fr>
2023-03-21 21:29:31 -0400

    Adding components
    - Lasers
    - Meteors
    - Score

--- asteroid_shooter_classes.py
+++ asteroid_shooter_classes.py
@@ -1,16 +1,75 @@
 import pygame, sys
+from random import randint, uniform
 
 class Ship(pygame.sprite.Sprite):
     def __init__(self, groups):
         super().__init__(groups)
         self.image = pygame.image.load('graphics/ship.png').convert_alpha()
         self.rect = self.image.get_rect(center = (WINDOW_WIDTH /2, WINDOW_HEIGHT /2))
+        self.can_shoot = True
+        self.shoot_time = None
+
+    def input_and_pos(self):
+        pos = pygame.mouse.get_pos()
+        self.rect.center = pos
+
+    def laser_shoot(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+            Laser(self.rect.midtop, laser_group)
+
+    def laser_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time > 500:
+                self.can_shoot = True
+
+    def update(self):
+        self.laser_timer()
+        self.laser_shoot()
+        self.input_and_pos()
 
 class Laser(pygame.sprite.Sprite):
-    def __init__(self, groups, ship):
+    def __init__(self, pos, groups):
         super().__init__(groups)
         self.image = pygame.image.load('graphics/laser.png').convert_alpha()
-        self.rect = self.image.get_rect(midbottom = ship.rect.midtop)
+        self.rect = self.image.get_rect(midbottom = pos)
+        
+        # float based position
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.direction = pygame.math.Vector2(0, -1)
+        self.speed = 600
+
+    def update(self):
+        self.pos += self.direction * self.speed * dt
+        self.rect.topleft = (round(self.pos.x), round(self.pos.y))
+
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load('graphics/meteor.png').convert_alpha()
+        self.rect = self.image.get_rect(center = pos)
+        
+        # float based position
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.direction = pygame.math.Vector2(uniform(-0.5, 0.5), 1)
+        self.speed = randint(400,600)
+
+    def update(self):
+        self.pos += self.direction * self.speed * dt
+        self.rect.topleft = (round(self.pos.x), round(self.pos.y))
+
+class Score:
+    def __init__(self):
+        self.font = pygame.font.Font('graphics/subatomic.ttf', 50)
+
+    def display(self):
+        text = f'Score: {pygame.time.get_ticks() // 1000}'
+        surf = self.font.render(text, True, (255, 255, 255))
+        rect = surf.get_rect(midbottom = (WINDOW_WIDTH /2, WINDOW_HEIGHT -80))
+        display_surface.blit(surf, rect)
+        pygame.draw.rect(display_surface, (255, 255, 255), rect.inflate(30, 30), width = 8, border_radius = 5)
 
 # basic setup
 pygame.init()
@@ -25,10 +84,17 @@ background_surface = pygame.image.load('graphics/background.png').convert()
 # sprite groups
 spaceship_group = pygame.sprite.GroupSingle()
 laser_group = pygame.sprite.Group()
+meteor_group = pygame.sprite.Group()
 
 # sprite creation
 ship = Ship(spaceship_group)
-laser = Laser(laser_group, ship)
+
+# score creation
+score = Score()
+
+# meteor timer
+meteor_timer = pygame.event.custom_type()
+pygame.time.set_timer(meteor_timer, 400)
 
 # game loop
 while True:
@@ -36,18 +102,32 @@ while True:
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.exit()
+            pygame.quit()
             sys.exit()
-    
+
+        if event.type == meteor_timer:
+            x_pos = randint(100, WINDOW_WIDTH - 100)
+            y_pos = randint(-100, -50)
+            Meteor(pos = (x_pos, y_pos), groups = meteor_group)
+
     # delta time
     dt = clock.tick() / 1000
 
     # backgrounds
     display_surface.blit(background_surface, (0,0))
 
+    # update
+    spaceship_group.update()
+    laser_group.update()
+    meteor_group.update()
+    
+    score.display()
+
     # graphics
     spaceship_group.draw(display_surface)
     laser_group.draw(display_surface)
+    meteor_group.draw(display_surface)
 
     # draw the frame
     pygame.display.update()
+
